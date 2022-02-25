c7b8996dc2495238cbcd22316b706f5eaf48becc
Nikita <73159874+Nikita06-dotcom@users.noreply.github.com>
2022-02-26 00:25:25 +0300

    Add files via upload

---
+++ registration/Formal_number.pas
@@ -0,0 +1,17 @@
+﻿var
+  c: longint;
+  s, s1: string;
+
+begin
+  read(s); c := 0;
+  
+  writeln('<table align="center" bgcolor="#202020" cellspacing="0" class="table_block"><tbody><tr>');
+  while 1 <= Length(s) do
+  begin
+    s1 := copy(s, 1, pos(',', s) - 1); delete(s, 1, pos(',', s) + 1); c += 1;
+    writeln('  <td><table align="center" bgcolor="#383838" cellspacing="0" class="table_block_2"><tbody><tr><td><p>', s1, '</p></td></tr></tbody></table></td>');
+    if c = 9 then
+    begin writeln('</tr></tbody></table> <table align="center" bgcolor="#202020" cellspacing="0" class="table_block"><tbody><tr>'); c := 0; end;  
+  end;
+  writeln('</tr></tbody></table><p> </p>');
+end.
